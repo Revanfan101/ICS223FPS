d0633d81762d6976555d85fbc65769bf36c1f9f5
Revanfan101 <98192268+Revanfan101@users.noreply.github.com>
2022-02-02 22:43:20 -0800

    Lab 2 Completed
    Lab 2 Completed

---
+++ ICS223 FPS/Assets/Scripts/Hole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+     void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Ball")
+            {
+                Destroy(collision.gameObject);
+            }
+
+    }
+}
---
+++ ICS223 FPS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbody;
+
+    private float speed = 10.0f;
+    //private float force = 250f;
+    float horizInput ;
+    float vertInput;
+
+    private void Start()
+    {
+        rbody = GetComponent < Rigidbody>();
+    }
+    private void Update()
+    {
+        horizInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector3 movement = new Vector3(vertInput, 0, horizInput) * speed * 100 *  Time.deltaTime; // * 100 to convert to newtons
+        //rbody.AddForce(movement);
+        rbody.velocity = movement;
+        //rbody.MovePosition(transform.position + movement);
+    }
+    //public float speed = 10f;
+    //// Update is called once per frame
+    //private void Update()
+    //{
+    //    float horizInput = Input.GetAxis("Horizontal");
+    //    float vertInput = Input.GetAxis("Vertical");
+    //    Vector3 movement = new Vector3(vertInput, 0, horizInput) * speed * Time.deltaTime;
+
+    //    transform.Translate(movement);
+    //}
+}
